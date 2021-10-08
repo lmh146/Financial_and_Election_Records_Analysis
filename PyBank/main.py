9e741be7b22cff1ab91f8d65cf9439fc5563172b
Lindsay Hwell <lmh146@pitt.edu>
2021-10-08 10:31:03 -0400

    Challenge Complete

---
+++ PyBank/main.py
@@ -0,0 +1,80 @@
+import os
+import csv
+
+from numpy import maximum
+
+#Refernce File
+PyBank_csv = os.path.join('Resources', 'budget_data.csv').replace("\\", "/")
+
+
+#Variables
+Months = []
+
+Ttl_ProfitLosses = 0
+
+Profit_Change = []
+Last_ProfitLoss = 0
+
+Date = []
+
+
+#Open File
+with open(PyBank_csv, 'r') as bankfile:
+    csv_reader = csv.reader(bankfile, delimiter=',')
+    
+    #Skip the First Line
+    header = next(csv_reader)
+
+
+    #For Loop to Extract Data
+    for row in csv_reader:
+
+        
+        #Month Count
+        Months.append(row[0])
+        
+
+        #Calculate Total Profits/Losses
+        Present_ProfitLoss = int(row[1])
+        Ttl_ProfitLosses = Ttl_ProfitLosses + Present_ProfitLoss
+
+
+        #Row to Row Change
+        Present_Change = Present_ProfitLoss - Last_ProfitLoss
+        Profit_Change.append(Present_Change)
+
+        Last_ProfitLoss = Present_ProfitLoss
+
+
+
+#Max and Min
+Maximum = max(Profit_Change)
+Max_Index = Profit_Change.index(Maximum)
+
+Max_Date = Months[Max_Index]
+
+    
+
+Minimum = min(Profit_Change)
+Min_Index = Profit_Change.index(Minimum)               
+
+Min_Date = Months[Min_Index]
+       
+
+
+
+#Finding the Average Change
+Profit_Change.pop(0)
+Average = round(int(sum(Profit_Change)) / int(len(Profit_Change)), 2)
+        
+
+        
+#Print Statements
+print('Finanacial Analysis')
+print('---------------------------------------------')
+print(f'Total Months: {len(Months)}')
+print(f'Total: ${Ttl_ProfitLosses}')
+print(f'Average Change: ${Average}')
+print(f'Greatest Increase in Profits: {Max_Date} (${Maximum})')
+print(f'Greatest Increase in Profits: {Min_Date} (${Minimum})')
+
---
+++ PyPoll/main.py
@@ -0,0 +1,82 @@
+import os
+import csv
+
+
+#Refernce File
+PyPoll_csv = os.path.join('Resources', 'election_data.csv').replace("\\", "/")
+
+
+#Variables
+Votes = []
+
+Candidates = ["Khan", "Coorey", "Li", "O'Tooley"]
+
+Total_Votes1 = 0
+Total_Votes2 = 0
+Total_Votes3 = 0
+Total_Votes4 = 0
+Prct_Votes = []
+
+
+
+#Open File
+with open(PyPoll_csv, 'r') as pollfile:
+    csv_reader = csv.reader(pollfile, delimiter=',')
+
+     #Skip the First Line
+    header = next(csv_reader)
+
+    for row in csv_reader:
+
+        #Total Votes
+        Votes.append(row[0])
+
+        #Votes per Candidates
+        candidate_choice = row[2]
+
+        if candidate_choice == "Khan":
+            Total_Votes1 = Total_Votes1 + 1
+
+        elif candidate_choice == "Correy":
+            Total_Votes2 = Total_Votes2 + 1
+
+        elif candidate_choice == "Li":
+            Total_Votes3 = Total_Votes3 + 1
+
+        else:
+            Total_Votes4 = Total_Votes4 + 1
+
+#Candidate's Precent of the Vote
+Overall_Votes = len(Votes)
+
+Can1_Percent = '{:.3f}'.format(round(((Total_Votes1/Overall_Votes) * 100), 3))
+Can2_Percent = '{:.3f}'.format(round(((Total_Votes2/Overall_Votes) * 100), 3))
+Can3_Percent = '{:.3f}'.format(round(((Total_Votes3/Overall_Votes) * 100), 3))
+Can4_Percent = '{:.3f}'.format(round(((Total_Votes4/Overall_Votes) * 100), 3))
+
+
+#Calculate the Winner
+if Can1_Percent > Can2_Percent and Can1_Percent > Can3_Percent and Can1_Percent > Can4_Percent:
+    Winner = "Khan"
+elif Can2_Percent > Can3_Percent and Can2_Percent > Can4_Percent and Can2_Percent > Can1_Percent:
+    Winner = "Correy"
+elif Can3_Percent > Can4_Percent and Can3_Percent > Can1_Percent and Can3_Percent > Can2_Percent:
+    Winner = "Li"
+else:
+    Winner = "O'Tooley"
+
+
+#Print Statements
+print("Election Results")
+print("---------------------------")
+print(f'Total Votes: {len(Votes)}')
+print("---------------------------")
+print(f"Khan: {Can1_Percent}% ({Total_Votes1})")
+print(f"Correy: {Can2_Percent}% ({Total_Votes2})")
+print(f"Li: {Can3_Percent}% ({Total_Votes3})")
+print(f"O'Tooley: {Can4_Percent}% ({Total_Votes4})")
+print("---------------------------")
+print(f"Winner: {Winner}")
+
+
+
